09287fcbebf74ad6f3e2d0492493536fa812361a
Eva Cernikova <eva.cr90@yahoo.com>
2015-09-07 13:19:28 +0200

    Pridani a uprava funkci

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-/*Pridani polozky*/
+/*Pridani polozky - tady to porad mizi a nevim proc*/
 $(document).ready(function() {
-	
 	$("button").click(function() {
-		$("ol").append("<li>"+$("input").val()+"</li>");
-		
-		$("input").val("");
+	$("ul").append("<li>"+$("input").val()+"</li>"); /*v materialech je ol misto ul, pak to nefunguje vubec*/
+	$("input").val("");
 	});
 });
-/*Mazani*/
-$(".task3").click(function() {
+
+/*Mazani kliknutim - toto ne*/
+$(".task").click(function() {
   $(this).slideUp();
 });
 
-/*Mazani tlacitkem*/
+/*Mazani tlacitkem - funguje, kdyz udelam ke kazde polozce vlastni class v html*/
 $(document).ready(function(){
     $("button").click(function(){
-        $("li").remove(".task3");
+        $("li").remove(".task");
     });
 });
 
-/*Zvyrazneni*/
+
+/*Zvyrazneni-funguje*/
 $(document).ready(function(){
-    $("li").mouseenter(function(){
-        $("li").css("text-color", "green");
-    });
-    $("li").mouseleave(function(){
-        $("li").css("text-color", "pink");
-    });
+ 	$("li").click(function(event) {
+  	$(event.target).closest("li").toggleClass( "hilight" );
+	});
 });
 
-/*Animace*/
+/*Animace - funguje, posune se o sekci dolů*/
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+    	$('html, body').animate({
+      	scrollTop: $(hash).offset().top
+    	}, 800, function(){
+   
+      window.location.hash = hash;
+    });
+  });
+});
+
 
 /*Edit*/
 
