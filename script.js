9bd42c1e28d680a1719b6b7c3746f45462d1bf8f
Eva Cernikova <eva.cr90@yahoo.com>
2015-09-07 07:24:31 +0200

    Update: jquery ukoly
    Zatim funguje jen mazani tlacitkem. Jinak tam mam asi neco blbe. Jeste to upgraduju, prip. dejte vedet, kdybyste vedeli, kde je chyba. Dekuju.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//TADY PRIDEJ JQUERY KOD PRO MANIPULOVANI S TO-DO LISTEM
+/*Pridani polozky*/
+$(document).ready(function() {
+	
+	$("button").click(function() {
+		$("ol").append("<li>"+$("input").val()+"</li>");
+		
+		$("input").val("");
+	});
+});
+/*Mazani*/
+$(".task3").click(function() {
+  $(this).slideUp();
+});
+
+/*Mazani tlacitkem*/
+$(document).ready(function(){
+    $("button").click(function(){
+        $("li").remove(".task3");
+    });
+});
+
+/*Zvyrazneni*/
+$(document).ready(function(){
+    $("li").mouseenter(function(){
+        $("li").css("text-color", "green");
+    });
+    $("li").mouseleave(function(){
+        $("li").css("text-color", "pink");
+    });
+});
+
+/*Animace*/
+
+/*Edit*/
+
