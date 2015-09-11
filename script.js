a260c00278aa2aa82886906910423f19cf7ff3d8
Jan Vlnas <git@jan.vlnas.cz>
2015-09-11 17:58:54 +0200

    3.7/8. krok: editace polozky misto smazani

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $("#add-task").click(function() {
     $(".todo-list").append("<li>"+$("input").val()+"</li>")
+    $("input").val("");
     // Vybereme naposledy pridanou polozku
     $(".todo-list li:last-child").click(function() {
       $(this).remove()
+      $("input").val($(this).text());
     });
-    $("input").val("");
   });
 
   // Kliknuti na polozku seznamu ji smaze
